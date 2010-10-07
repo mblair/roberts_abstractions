804e5707b23dd3071d5e07d89a93f9c9f741fa44
Matt Blair <me@matthewblair.net>
2010-10-07 18:24:38 -0400

    The book would prefer this...

---
+++ ch1/even_or_odd.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "../book_code/unix-xwindows/genlib.h"
+#include "../book_code/unix-xwindows/simpio.h"
+
+int main() {
+	int n;
+
+	printf("This program labels a number as even or odd.\n");
+	printf("Enter a number: ");
+	n = GetInteger();
+	if (n % 2 == 0) {
+		printf("That number is even.\n");
+	} else {
+		printf("That number is odd.\n");
+	}
+
+	return 0;
+}
