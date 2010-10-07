51d3f5be8cf1dcd3c6fb35e9d2c14158a933fa8a
Matt Blair <me@matthewblair.net>
2010-10-07 19:44:43 -0400

    Neat, sum of the digits in an integer.

---
+++ ch1/digitsum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int DigitSum(int n) {
+	int sum = 0;
+
+	while (n > 0) {
+		sum += n % 10;
+		n /= 10;
+	}
+
+	return (sum);
+}
+
+int main() {
+	int num;
+
+	printf("Please enter an integer: ");
+	scanf("%d", &num);
+	printf("The sum of the digits in %d is %d.\n", num, DigitSum(num));
+
+	return 0;
+}
