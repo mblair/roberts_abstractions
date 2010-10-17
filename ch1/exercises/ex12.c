8213ebd1bd3f1655c30b1e929496326e808daa63
Matt Blair <me@matthewblair.net>
2010-10-16 23:34:53 -0400

    Done with #12.

--- ch1/exercises/ex12.c
+++ ch1/exercises/ex12.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
-//#include "../../book_code/unix-xwindows/genlib.h"
+
+void printTens(int x) {
+	switch(x) {
+		case 2: printf("twenty"); return;
+		case 3: printf("thirty"); return;
+		case 4: printf("fourty"); return;
+		case 5: printf("fifty"); return;
+		case 6: printf("sixty"); return;
+		case 7: printf("seventy"); return;
+		case 8: printf("eighty"); return;
+		case 9: printf("ninety"); return;
+	}
+}
 
 void printOneDigit(int x) {
 	switch(x) {
-		case 0: printf("zero"); break;
-		case 1: printf("one"); break;
-		case 2: printf("two"); break;
-		case 3: printf("three"); break;
-		case 4: printf("four"); break;
-		case 5: printf("five"); break;
-		case 6: printf("six"); break;
-		case 7: printf("seven"); break;
-		case 8: printf("eight"); break;
-		case 9: printf("nine"); break;
-		default: exit(1); break;
+		case 1: printf("one"); return;
+		case 2: printf("two"); return;
+		case 3: printf("three"); return;
+		case 4: printf("four"); return;
+		case 5: printf("five"); return;
+		case 6: printf("six"); return;
+		case 7: printf("seven"); return;
+		case 8: printf("eight"); return;
+		case 9: printf("nine"); return;
 	}
 }
 
@@ -38,19 +47,64 @@ int lastThree(int x) {
 	return x % 1000;
 }
 
+int lastTwo(int x) {
+	return x % 100;
+}
+
 void printNumber(int x) {
+	if(x >= 0 && x < 10) {
+		printOneDigit(x);
+		return;
+	}
+
 	if (hundreds(x) > 0) {
 		printOneDigit(hundreds(x));
-		printf(" hundred");
+		printf(" hundred ");
 	}
+
+	if((lastTwo(x) >= 10 && lastTwo(x) <= 13) || lastTwo(x) == 15 || lastTwo(x) == 18) {
+		switch(lastTwo(x)) {
+			case 10: printf("ten"); return;
+			case 11: printf("eleven"); return;
+			case 12: printf("twelve"); return;
+			case 13: printf("thirteen"); return;
+			case 15: printf("fifteen"); return;
+			case 18: printf("eighteen"); return;
+		}
+	}
+
+	if(lastTwo(x) == 14 || lastTwo(x) == 16 || lastTwo(x) == 17 || lastTwo(x) == 19) { //Yikes, I know. But this is the reusable functions question.
+		printOneDigit(x % 10);
+		printf("teen");
+		return;
+	}
+
+	if(tens(x) > 1) {
+		printTens(tens(x));
+		if (ones(x) > 0) {
+			printf("-");
+			printOneDigit(ones(x));
+		}
+		return;
+	}
+	
+	printOneDigit(ones(x));
+	return;
+
 }
 
 void printTotal(int x) {
+	if(x == 0) {
+		printf("zero");
+		return;
+	}
+
 	if(thousands(x) > 0) {
 		printNumber(thousands(x));
 		printf(" thousand ");
 	}
 	printNumber(lastThree(x));
+	return;
 }
 
 int main(void) {
